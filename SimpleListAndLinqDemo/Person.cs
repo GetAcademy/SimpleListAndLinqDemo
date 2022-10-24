60876e8cbe7681c11d35a7246dfd2ee0aa7982ce
GET Academy <terje@getacademy.no>
2022-10-24 13:28:35 +0200

    Initial commit

---
+++ SimpleListAndLinqDemo/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace SimpleListAndLinqDemo
+{
+    internal class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int PostalCode { get; set; }
+        public int Age => (int)(DateTime.Now - BirthDay).TotalDays / 365;
+
+        public Person(int id, string firstName, string lastName, DateTime birthDay, int postalCode)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDay = birthDay;
+            PostalCode = postalCode;
+        }
+
+        public Person()
+        {
+        }
+    }
+}
